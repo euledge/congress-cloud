68ef11635a0ef9833cded069dc7589c0b2b788fc
euledge <hitoshi.kuroyanagi@gmail.com>
2021-10-18 21:43:44 +0900

    環境変数からの読み取り

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Amplify from 'aws-amplify'
-import awsExports from '../src/aws-exports'
+
+const awsExports = {
+    "aws_project_region": process.env.AWS_PROJECT_REGION,
+    "aws_appsync_graphqlEndpoint": process.AWS_APPSYNC_GRAPHQLENDPOINT,
+    "aws_appsync_region": process.AWS_APPSYNC_REGION,
+    "aws_appsync_authenticationType": process.AWS_APPSYNC_AUTHENTICATIONTYPE,
+    "aws_appsync_apiKey": process.AWS_APPSYNC_APIKEY
+}
 
 Amplify.configure(awsExports)
 Vue.use(Amplify)
