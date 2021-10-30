de1b1f83dfac309dce2559daccee89ce75ceb404
euledge <hitoshi.kuroyanagi@gmail.com>
2021-10-30 11:11:54 +0900

    build settings追加

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -2,11 +2,11 @@ import Vue from 'vue'
 import Amplify from 'aws-amplify'
 
 const awsExports = {
-    "aws_project_region": process.env.AWS_PROJECT_REGION,
-    "aws_appsync_graphqlEndpoint": process.AWS_APPSYNC_GRAPHQLENDPOINT,
-    "aws_appsync_region": process.AWS_APPSYNC_REGION,
-    "aws_appsync_authenticationType": process.AWS_APPSYNC_AUTHENTICATIONTYPE,
-    "aws_appsync_apiKey": process.AWS_APPSYNC_APIKEY
+    "aws_project_region": process.env.ENV_PROJECT_REGION,
+    "aws_appsync_graphqlEndpoint": process.ENV_APPSYNC_GRAPHQLENDPOINT,
+    "aws_appsync_region": process.ENV_APPSYNC_REGION,
+    "aws_appsync_authenticationType": process.ENV_APPSYNC_AUTHENTICATIONTYPE,
+    "aws_appsync_apiKey": process.ENV_APPSYNC_APIKEY
 }
 
 Amplify.configure(awsExports)
